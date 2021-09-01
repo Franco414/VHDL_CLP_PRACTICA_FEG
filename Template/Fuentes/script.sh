be826ad499c1affeb6253c182adb7892b160f539
Franco414 <franco.alfa76@gmail.com>
2021-09-01 01:33:08 -0300

    Update script in template

--- Template/Fuentes/script.sh
+++ Template/Fuentes/script.sh
@@ -1,6 +1,6 @@
 NAMECOMP="nombre"
 ghdl -a ../Fuentes/$NAMECOMP.vhd ..//Fuentes/${NAMECOMP}_tb.vhd
-ghdl -s ../Fuentes/&NAMECOMP.vhd ..//Fuentes/${NAMECOMP}_tb.vhd
+ghdl -s ../Fuentes/$NAMECOMP.vhd ..//Fuentes/${NAMECOMP}_tb.vhd
 ghdl -e ${NAMECOMP}_tb
 ghdl -r ${NAMECOMP}_tb --vcd=${NAMECOMP}_tb.vcd --stop-time=1000ns
 gtkwave ${NAMECOMP}_tb.vcd
